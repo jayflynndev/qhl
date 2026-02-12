e56b55d30daa8f231bd4d9bc9925d7271eee69e0
jayflynndev <jayflynndev@outlook.com>
2026-02-12 17:42:08 +0000

    0.12

--- app/admin/venues/page.tsx
+++ app/admin/venues/page.tsx
@@ -1,5 +1,7 @@
 import { AdminVenuesPage } from "@/components/qhl/admin/venues/AdminVenuesPage";
 
+export const dynamic = "force-dynamic";
+
 export default function Page() {
   return <AdminVenuesPage />;
 }
