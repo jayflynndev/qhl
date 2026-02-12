0e5dc1ff3887b2a276bd9129d17b5f6b32e5d096
jayflynndev <jayflynndev@outlook.com>
2026-02-12 17:38:54 +0000

    0.10

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { AdminQhlHub } from "@/components/qhl/admin/AdminQhlHub";
 
+export const dynamic = "force-dynamic";
+
 export default function Page() {
   return <AdminQhlHub />;
 }
